2e2210f180d92bbd4821367f3fcc5f017c587a15
Gerald <macbook@MACBOOKs-MacBook-Air.local>
2025-06-20 13:56:09 +0100

    Added bio web page

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
+// app.js
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello from Render CI/CD!'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => console.log(`App running on port ${port}`));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
+
